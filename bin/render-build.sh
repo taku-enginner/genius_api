c265a868a00b545f21a18b911b5ffef060c08e11
taka4655 <taku.enginner@gmail.com>
2025-01-27 19:58:50 +0900

    renderビルドエラー解消

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,12 @@
 set -o errexit
 
 bundle install
+yarn install # 追加: Yarnを使用している場合
+npm install  # 追加: npmを使用している場合
+
+# Babelプラグインのインストール
+npm install --save-dev @babel/plugin-proposal-private-methods
+
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
