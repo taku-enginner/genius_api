5f0ace95d6a07c8984ba656384b42089ecaf2c13
taka4655 <taku.enginner@gmail.com>
2025-01-27 20:30:31 +0900

    エラー解消

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,9 @@
 set -o errexit
 
-bundle install
+export NODE_OPTIONS=--openssl-legacy-provider
 
+bundle install
+yarn install # Yarnを使用している場合
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
